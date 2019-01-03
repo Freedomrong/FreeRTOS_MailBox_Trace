29021b1ebea05e7769dbf2144337035cf0f113f0
hujiaodigua <371629179@qq.com>
2019-01-03 19:41:25 +0800

    finished script.py of timestamp analyzing

--- Unix_Timestamp/script.py
+++ Unix_Timestamp/script.py
@@ -25,22 +25,23 @@ def find_closest(L, target): #L
                 return i
                 break
 
+file_Mark_num = '0'
 
 '''STEP1'''
-input_file = '0-Mail_Box_Tracing.vcd'
-output_txt_file = '0data-Mail_Box_Tracing.txt'
+input_file = file_Mark_num + '-Mail_Box_Tracing.vcd'
+output_txt_file = file_Mark_num + 'data-Mail_Box_Tracing.txt'
 
 vcd2txt_command = ('sigrok-cli -I vcd -i ' + input_file + ' >> ' + output_txt_file)
 
 response = subprocess.getstatusoutput(vcd2txt_command)
 
-input_file = '0-Mail_Box_Tracing.vcd'
-output_txt_decode_file = '0decode-data-Mail_Box_Tracing.txt'
+input_file = file_Mark_num + '-Mail_Box_Tracing.vcd'
+output_txt_decode_file = file_Mark_num + 'decode-data-Mail_Box_Tracing.txt'
 decode_command = ('sigrok-cli -I vcd -i ' + input_file + ' -P uart:rx=SWO:baudrate=8000000,arm_tpiu:stream=1,arm_itm' + ' >> ' + output_txt_decode_file)
 response = subprocess.getstatusoutput(decode_command)
 
 
-output_txt_itm_file = '0-itm-decode-data-Mail_Box_Tracing.txt'
+output_txt_itm_file = file_Mark_num + '-itm-decode-data-Mail_Box_Tracing.txt'
 response = subprocess.getstatusoutput("grep -rn 'itm' " + output_txt_decode_file + " >> " + output_txt_itm_file)
 
 
@@ -48,7 +49,7 @@ response = subprocess.getstatusoutput("grep -rn 'itm' " + output_txt_decode_file
 outputTick_txt_file = ('Tick-' + output_txt_file)
 grepTick_command = ("grep -rn 'Tick'" + output_txt_file + ' >> ' + outputTick_txt_file)    # 在windows中安装了'GREP for windows' -- http://www.interlog.com/~tcharron/grep.html
 # response = subprocess.getstatusoutput(grepTick_command)
-subprocess.getstatusoutput("grep -rn 'Tick' 0data-Mail_Box_Tracing.txt >> Tick-0data-Mail_Box_Tracing.txt")
+subprocess.getstatusoutput("grep -rn 'Tick' " + file_Mark_num + "data-Mail_Box_Tracing.txt >> Tick-" +file_Mark_num + "data-Mail_Box_Tracing.txt")
 
 file_object = open(outputTick_txt_file, 'r')
 try:
@@ -85,15 +86,15 @@ for n in range(0,len(data_spilt3_lines)):    # 最外层
             data_divided.append(data_spilt3_lines[n][i][k])
             # print(data_spilt3_lines[n][i][k])             # data_spilt3_lines[0]有64个字符
 
-for i in range(0,len(data_divided)):
-    if data_divided[i] == '1':
-        if data_divided[i + 1] == '1':
-            if data_divided[i + 1 + 1] == '1':
-                # print(i)
-                zero2one_gap = i    # 1个间隔是1ns
-                break
+counter_Tick = []
+for i in range(0,len(data_divided) - 1):
+    if data_divided[i] != data_divided[i+1]:
+        counter_Tick.append(i)    # 1个间隔是1ns
 
-print('从开始到第1个Tick上升沿所经历的时间为:%fs' % ((zero2one_gap * 1)/1000000000))
+
+firstedge_tick_gap = counter_Tick[0]
+print("Tick的个数为:%d" % (len(counter_Tick)))
+print('从开始到第1个Tick上升/下降沿所经历的时间为:%fs' % ((firstedge_tick_gap * 1)/1000000000))
 
 '''STEP3'''
 Mark_file = 'Mark.txt'
@@ -123,7 +124,7 @@ for i in range(0,len(Mark_spilt2_lines)):
     Mark_timestamp.append(temp3)
 
 '''STEP4'''
-GPIOtimestamp_file = '0-GPIO_Timestamp.txt'
+GPIOtimestamp_file = file_Mark_num + '-GPIO_Timestamp.txt'
 
 file_object = open(GPIOtimestamp_file, 'r')
 try:
@@ -143,7 +144,8 @@ for i in range(0,len(GPIO_spilt1_lines)):
     temp2 = GPIO_spilt1_lines[i].strip('\n')    # 20181231 删除\n
     GPIO_timestamp.append(float(temp2))
 
-index_WP0GPIOtimestamp_first = find_closest(GPIO_timestamp, Mark_timestamp[0] + (zero2one_gap * 1)/1000000000)
+print("预估第一个Tick的时间为：%fs" %(Mark_timestamp[int(file_Mark_num)] + (firstedge_tick_gap * 1)/1000000000))
+index_WP0GPIOtimestamp_first = find_closest(GPIO_timestamp, Mark_timestamp[int(file_Mark_num)] + (firstedge_tick_gap * 1)/1000000000)
 WP0GPIOtimestamp_first = GPIO_timestamp[index_WP0GPIOtimestamp_first]
 print("在GPIO捕获的Tick时间中与预估时间最为接近的时间：%fs" %WP0GPIOtimestamp_first)
 
@@ -166,11 +168,22 @@ for i in range(0,len(itm_data_lines)):
     if itm_data_lines[i].find(target_str) != -1:
         index_WP0.append(i)
 
+print("抓GPIO的Watchpoint0的个数：%d" % len(index_WP0))
+
 for i in range(0,len(index_WP0)):
     temp4 = itm_data_lines[index_WP0[i]]
-    itm_data_lines[index_WP0[i]] = temp4.strip('\n') + " --" + str(GPIO_timestamp[index_WP0GPIOtimestamp_first + i]) + "\n"
+    #itm_data_lines[index_WP0[i]] = temp4.strip('\n') + " --" + str(GPIO_timestamp[index_WP0GPIOtimestamp_first + i]) + "\n"
+    if i == 0:
+        itm_data_lines[index_WP0[i]] = temp4.strip('\n') + " --" + str(WP0GPIOtimestamp_first + 0) + "\n"    #
+    if i > 0:
+        temp5 = round(counter_Tick[i] - counter_Tick[0] ,13)
+        temp6 = round(((temp5 * 1) / 1000000000), 13)
+        temp7 = round(WP0GPIOtimestamp_first , 13)
+        temp_timestamp = round(temp7 + temp6, 13)
+        itm_data_lines[index_WP0[i]] = temp4.strip('\n') + " --" + str(temp_timestamp) + "\n"    # counter_Tick记录的第一个值是0到第一个Tick边沿的时间，这个时间已经用来计算WP0GPIOtimestamp_first了
 
-output_txt_file = '0-addGPIOtimestamp-itm-decode-data-Mail_Box_Tracing.txt'    # 完成一个文件的时间对齐操作
+
+output_txt_file = file_Mark_num + '-addGPIOtimestamp-itm-decode-data-Mail_Box_Tracing.txt'    # 完成一个文件的时间对齐操作
 file_object = open(output_txt_file, 'w')
 file_object.writelines(itm_data_lines)
 file_object.close()
